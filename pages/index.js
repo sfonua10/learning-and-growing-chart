403f78d7c048cde93f29ec91933151b9aa3f164e
Saia Fonua <saiafonua@users-MacBook-Pro.local>
2021-01-31 23:17:07 -0700

    test dates in prod

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,63 @@
 import Head from "next/head";
 
 export default function Home() {
+  const naturalManResponses = [
+    {
+      term: "Blame others",
+      definition: "Assign responsibilty for a fault or wrong",
+      part_of_speech: 'verb'
+    },
+    {
+      term: "Make excuses",
+      definition: "attempt to lessen the blame attaching to (a fault or offense); seek to defend or justify",
+      part_of_speech: 'verb'
+    },
+    {
+      term: "Rebel",
+      definition: "rise in opposition or armed resistance to an established government or ruler",
+      part_of_speech: 'verb'
+    },
+    {
+      term: "Complain",
+      definition: "Express dissatisfaction or annoyance about something",
+      part_of_speech: 'verb'
+    },
+    {
+      term: "Find fault",
+      definition: "Make an adverse criticism or objection, sometimes unfairly or destructively",
+      part_of_speech: 'verb'
+    },
+    {
+      term: "Doubt",
+      definition: "feel uncertain about",
+      part_of_speech: 'verb'
+    },
+    {
+      term: "Give up",
+      definition: "Cease making an effort; resign oneself to failure",
+      part_of_speech: 'verb'
+    },
+    {
+      term: "Get angry",
+      definition: "feeling or showing strong annoyance, displeasure, or histility; full of anger",
+      part_of_speech: 'verb'
+    },
+    {
+      term: "Indult in self-pity",
+      definition: "Excessive, self-absorbed unhappiness over one's own troubles",
+      part_of_speech: 'verb'
+    },
+    {
+      term: "Allow fear to rule",
+      definition: "An unpleasant emotion caused by the belief that someone or something is dangerous, likely to cause pain, or a threat",
+      part_of_speech: 'verb'
+    },
+    {
+      term: "Self-justification",
+      definition: "The justification or excusing of oneself or one's actions",
+      part_of_speech: 'noun'
+    },
+  ]
   const dummyData = [
     {
       situation: "Wife and I disagree if Skribbl.io is appropriate or not",
@@ -28,6 +85,8 @@ export default function Home() {
       ],
     },
   ];
+  const todayDate = new Date().toLocaleDateString();
+
   return (
     <div>
       <Head>
@@ -36,6 +95,15 @@ export default function Home() {
       </Head>
 
       <main>
+        <div>
+          <p>{todayDate}</p>
+        </div>
+        <div>
+          <h1>EXAMPLE NATURAL MAN RESPONSES</h1>
+          <ul className="list-disc">
+            {naturalManResponses.map(nmr => <li className="list-disc">{nmr.term} - {nmr.definition}</li>)}   
+          </ul>
+        </div>
         <table className="ml-4 mr-4">
           <thead>
             <tr>
