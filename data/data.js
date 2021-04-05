b6ddd212b3ebf48e440baf60ae18b6d24d74f1bf
Saia Fonua <saia.fonua@C02F20S7MD6M.local>
2021-04-04 21:52:10 -0600

    abstract data, hide list, add new func

---
+++ data/data.js
@@ -0,0 +1,85 @@
+export const naturalManResponses = [
+    {
+        term: "Blame others",
+        definition: "Assign responsibilty for a fault or wrong",
+        part_of_speech: 'verb'
+    },
+    {
+        term: "Make excuses",
+        definition: "attempt to lessen the blame attaching to (a fault or offense); seek to defend or justify",
+        part_of_speech: 'verb'
+    },
+    {
+        term: "Rebel",
+        definition: "rise in opposition or armed resistance to an established government or ruler",
+        part_of_speech: 'verb'
+    },
+    {
+        term: "Complain",
+        definition: "Express dissatisfaction or annoyance about something",
+        part_of_speech: 'verb'
+    },
+    {
+        term: "Find fault",
+        definition: "Make an adverse criticism or objection, sometimes unfairly or destructively",
+        part_of_speech: 'verb'
+    },
+    {
+        term: "Doubt",
+        definition: "feel uncertain about",
+        part_of_speech: 'verb'
+    },
+    {
+        term: "Give up",
+        definition: "Cease making an effort; resign oneself to failure",
+        part_of_speech: 'verb'
+    },
+    {
+        term: "Get angry",
+        definition: "feeling or showing strong annoyance, displeasure, or histility; full of anger",
+        part_of_speech: 'verb'
+    },
+    {
+        term: "Indult in self-pity",
+        definition: "Excessive, self-absorbed unhappiness over one's own troubles",
+        part_of_speech: 'verb'
+    },
+    {
+        term: "Allow fear to rule",
+        definition: "An unpleasant emotion caused by the belief that someone or something is dangerous, likely to cause pain, or a threat",
+        part_of_speech: 'verb'
+    },
+    {
+        term: "Self-justification",
+        definition: "The justification or excusing of oneself or one's actions",
+        part_of_speech: 'noun'
+    },
+]
+
+export const dummyData = [
+    {
+        situation: "Wife and I disagree if Skribbl.io is appropriate or not",
+        NMR: [
+            "Get angry and blame others",
+            "Doubt your abilities",
+            "Lose hope you'll find a good job and quit trying",
+        ],
+        WAR: [
+            "Ask for feedback from others, Find new skills you can learn or thing that would give you more experience.",
+            "Apply for other jobs",
+        ],
+    },
+    {
+        situation: "I'm feeling overwhelmed talking about duties/responsibilities, etc. I feel like I'm being constantly nagged by wife",
+        NMR: [
+            "Get angry and know that I'm right",
+            "Doubt your abilities",
+            "Lose hope you'll find a good job and quit trying",
+        ],
+        WAR: [
+            "Ask for feedback from others.",
+            "Find new skills you can learn or thing that would give you more experience.",
+            "Apply for other jobs",
+        ],
+    },
+];
--- pages/index.js
+++ pages/index.js
@@ -1,92 +1,32 @@
+import { useState } from 'react';
 import Head from "next/head";
+import { naturalManResponses, dummyData } from '../data/data';
 
 export default function Home() {
-  const naturalManResponses = [
-    {
-      term: "Blame others",
-      definition: "Assign responsibilty for a fault or wrong",
-      part_of_speech: 'verb'
-    },
-    {
-      term: "Make excuses",
-      definition: "attempt to lessen the blame attaching to (a fault or offense); seek to defend or justify",
-      part_of_speech: 'verb'
-    },
-    {
-      term: "Rebel",
-      definition: "rise in opposition or armed resistance to an established government or ruler",
-      part_of_speech: 'verb'
-    },
-    {
-      term: "Complain",
-      definition: "Express dissatisfaction or annoyance about something",
-      part_of_speech: 'verb'
-    },
-    {
-      term: "Find fault",
-      definition: "Make an adverse criticism or objection, sometimes unfairly or destructively",
-      part_of_speech: 'verb'
-    },
-    {
-      term: "Doubt",
-      definition: "feel uncertain about",
-      part_of_speech: 'verb'
-    },
-    {
-      term: "Give up",
-      definition: "Cease making an effort; resign oneself to failure",
-      part_of_speech: 'verb'
-    },
-    {
-      term: "Get angry",
-      definition: "feeling or showing strong annoyance, displeasure, or histility; full of anger",
-      part_of_speech: 'verb'
-    },
-    {
-      term: "Indult in self-pity",
-      definition: "Excessive, self-absorbed unhappiness over one's own troubles",
-      part_of_speech: 'verb'
-    },
-    {
-      term: "Allow fear to rule",
-      definition: "An unpleasant emotion caused by the belief that someone or something is dangerous, likely to cause pain, or a threat",
-      part_of_speech: 'verb'
-    },
-    {
-      term: "Self-justification",
-      definition: "The justification or excusing of oneself or one's actions",
-      part_of_speech: 'noun'
-    },
-  ]
-  const dummyData = [
-    {
-      situation: "Wife and I disagree if Skribbl.io is appropriate or not",
-      NMR: [
-        "Get angry and blame others",
-        "Doubt your abilities",
-        "Lose hope you'll find a good job and quit trying",
-      ],
-      WAR: [
-        "Ask for feedback from others, Find new skills you can learn or thing that would give you more experience.",
-        "Apply for other jobs",
-      ],
-    },
-    {
-      situation: "I'm feeling overwhelmed talking about duties/responsibilities, etc. I feel like I'm being constantly nagged by wife",
-      NMR: [
-        "Get angry and know that I'm right",
-        "Doubt your abilities",
-        "Lose hope you'll find a good job and quit trying",
-      ],
-      WAR: [
-        "Ask for feedback from others.",
-        "Find new skills you can learn or thing that would give you more experience.",
-        "Apply for other jobs",
-      ],
-    },
-  ];
+  const [showList, setShowList] = useState(false);
+  const [situations, setSituations] = useState(dummyData);
   const todayDate = new Date().toLocaleDateString();
 
+  const hideList = () => {
+    setShowList(!showList)
+  }
+
+  const addSituation = () => {
+    let newSituation = {
+      situation: 'test',
+      NMR: [
+        'test',
+        'test',
+        'test'
+      ],
+      WAR: [
+        'test',
+        'test',
+        'test'
+      ]
+    }
+    setSituations([...situations, newSituation])
+  }
   return (
     <div>
       <Head>
@@ -94,17 +34,21 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
+      <main className="ml-4 mr-4">
         <div>
           <p>{todayDate}</p>
         </div>
-        <div>
-          <h1>EXAMPLE NATURAL MAN RESPONSES</h1>
-          <ul className="list-disc">
-            {naturalManResponses.map(nmr => <li className="list-disc">{nmr.term} - {nmr.definition}</li>)}   
-          </ul>
-        </div>
-        <table className="ml-4 mr-4">
+        <button className="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" onClick={hideList}>{showList ? 'Hide List' : 'Show List'}</button>
+        {
+          showList &&
+          <div>
+            <h1>EXAMPLE NATURAL MAN RESPONSES</h1>
+            <ul className="list-disc">
+              {naturalManResponses.map(nmr => <li className="list-disc">{nmr.term} - {nmr.definition}</li>)}
+            </ul>
+          </div>
+        }
+        <table >
           <thead>
             <tr>
               <th className="w-1/4 px-4 py-2 text-blue-600">Situation</th>
@@ -117,18 +61,18 @@ export default function Home() {
             </tr>
           </thead>
           <tbody>
-            {dummyData.map((sit, index) => {
-              console.log('index', index);
+            {situations?.map((sit, index) => {
               return (
                 <tr className={`${index % 2 === 1 ? 'bg-blue-200' : 'bg-blue-100'}`}>
-                  <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">{sit.situation}</td>
-                  <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">{sit.NMR.map(x => (<p>{x}</p>))}</td>
-                  <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">{sit.WAR.map(x => (<p>{x}</p>))}</td>
+                  <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">{sit?.situation}</td>
+                  <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">{sit?.NMR?.map(x => (<p>{x}</p>))}</td>
+                  <td className="border border-blue-500 px-4 py-2 text-blue-600 font-medium">{sit?.WAR?.map(x => (<p>{x}</p>))}</td>
                 </tr>
               );
             })}
           </tbody>
         </table>
+          <button onClick={addSituation}>Add New</button>
       </main>
     </div>
   );
